59ef6430a43be674baac8b4b71789d5cb53f0455
roarty34-creator <roarty34@gmail.com>
2026-02-26 23:27:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = "fishlog-cache-v1";
+const CACHE_NAME = "fishlog-cache-v3";
 
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
-];
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
 });
