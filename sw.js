7af9523a1f2d68425a4b703be2543966614b0281
roarty34-creator <roarty34@gmail.com>
2026-03-02 22:15:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
 
+const CACHE = "fishlog-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
