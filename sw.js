a7acb7e09bf53ec9c6326d6acb62b32c81b419e5
roarty34-creator <roarty34@gmail.com>
2026-03-03 17:24:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+const CACHE = "nicoleen-fishlog-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./sw.js"
+];
 
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
