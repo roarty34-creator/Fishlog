aa62fe3bd71bb0b4d241d30fa457ffc40598ce21
roarty34-creator <roarty34@gmail.com>
2026-03-04 21:10:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,41 @@
-const CACHE = "fishlog-v2";
+const CACHE = "fishlog-v3";
+
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon-192.png",
-  "./icon-512.png",
-  "./sw.js"
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE) return caches.delete(k);
+        })
+      )
     )
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
+
+  // laat map tiles en CDN scripts direk deurgaan
+  if (
+    e.request.url.includes("tile.openstreetmap.org") ||
+    e.request.url.includes("unpkg.com")
+  ) {
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
